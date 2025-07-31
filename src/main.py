27e9c2e5f8b9c7aded92109aeeb8a180a1b41bb0
Alex Berka <alexanderberka@gmail.com>
2025-07-31 09:31:30 -0500

    WIP

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from src.dependencies.logger_init import setup_logging
 from src.crud.us_deathcounts import fetch_us_deathcounts
 from src.crud.uk_cases_by_day import fetch_uk_cases_by_day
+from src.crud.us_deathcounts import fetch_us_deathcounts
 
 app = FastAPI()
 
@@ -14,6 +15,17 @@ async def read_root():
         "message": "Welcome to the FastAPI demo! Visit /docs for API documentation and how to use this."
     }
 
+@app.get("/databases/")
+async def get_all_databases(country: str = None, keyword: str = None, last_updated: int = None):
+    logger.info(f"Databases endpoint accessed with query parameters: country={country}, keyword={keyword}, last_updated={last_updated}")
+
+    return
+
+@app.get("/databases/{database_id}")
+async def get_single_database(database_id: int, limit: int = 20, offset: int = 0):
+    logger.info(f"Single database endpoint at for database_id={database_id} accessed with query parameters: limit={limit}, offset={offset}")
+
+    return
 
 @app.get("/test-endpoint")
 async def fetch_test_endpoint():
